fabcad31e290a7501f58fbcfa0823ba8df9fac60
Kashish <kashishverma8382@gmail.com>
2022-09-22 08:12:10 +0530

    ✌️

---
+++ src/components/AddNote.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useState,useContext } from 'react';
+import eContext from '../context/notes/NoteContext';
+export default function AddNote() {
+    const context = useContext(eContext);
+    const{addNote}=context;
+    const [note, setNote] = useState({title :"",description:"",tag:"default"})
+    const handleClick=(e)=>{
+        e.preventDefault();
+addNote(note.title,note.description,note.tag);
+    }
+
+
+    const onChange=(e)=>{
+setNote({...note,[e.target.name]:e.target.value})
+    }
+  return (
+
+    <div>   <div className="container my-3">
+    <h2>Add a Note</h2>
+    <form>
+  <div className="mb-3">
+    <label for="title" className="form-label">title</label>
+    <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" onChange={onChange}/>
+
+  </div>
+  <div className="mb-3">
+    <label for="description" className="form-label">Description</label>
+    <input type="text" className="form-control" id="description" name="description" onChange={onChange}/>
+  </div>
+  <div className="mb-3 form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
+</form>
+
+    </div></div>
+  )
+}
